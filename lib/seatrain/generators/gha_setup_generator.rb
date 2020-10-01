37bf12b1c92ce1d13f41a5635a39144ceaef081f
Andy B <andybarnov@gmail.com>
2020-10-02 00:07:46 +0200

    minor

--- lib/seatrain/generators/gha_setup_generator.rb
+++ lib/seatrain/generators/gha_setup_generator.rb
@@ -23,7 +23,8 @@ module Seatrain
 
     # TODO:
     def print_instructions
-      prompt.warn "⚠️  You need to set the following secrets in your GitHub repository: "
+      prompt.warn "⚠️  You need to set the following secrets in your GitHub repository: \n"
+      prompt.warn "👉  DIGITALOCEAN_ACCESS_TOKEN"
       prompt.warn "👉  DOCKER_USERNAME" unless Seatrain.config.uses_docr? || Seatrain.config.docker_username.empty?
       prompt.warn "👉  DOCKER_PASSWORD" unless Seatrain.config.uses_docr?
       prompt.warn "👉  RAILS_MASTER_KEY"
